716975d4e9fa448a1084f7df48676761f98f9354
Ateto1202 <tonysu1204@gmail.com>
2024-12-21 23:16:17 +0800

    Upd index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,12 @@ import ProductCard from '../app/component/ProductCard';
 import { Product } from '../app/model/product';
 import NavigationBar from '../app/component/NavigationBar';
 import { useEffect, useState } from 'react';
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import '../globals.css';
+import Image from 'next/image';
 
 export const getServerSideProps: GetServerSideProps = async () => {
     try {
@@ -19,6 +24,40 @@ export const getServerSideProps: GetServerSideProps = async () => {
         console.error("Error fetching data:", err);
         return { props: { data: [] } };
     }
+};
+
+const CustomPrevArrow = ({ onClick }: { onClick?: () => void }) => {
+    return (
+        <button
+            className="absolute left-[-40px] top-1/2 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full shadow hover:bg-gray-700"
+            onClick={onClick}
+        >
+            ←
+        </button>
+    );
+};
+
+const CustomNextArrow = ({ onClick }: { onClick?: () => void }) => {
+    return (
+        <button
+            className="absolute right-[-40px] top-1/2 transform -translate-y-1/2 z-10 bg-gray-800 text-white p-2 rounded-full shadow hover:bg-gray-700"
+            onClick={onClick}
+        >
+            →
+        </button>
+    );
+};
+
+const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
 };
 
 function Home({ data }: { data: Product[] }) {
@@ -44,7 +83,41 @@ function Home({ data }: { data: Product[] }) {
     return (
         <>
             <NavigationBar />
-            <div className="container mx-auto px-4 py-8 mt-24">
+            <div className="mx-auto my-6 mt-32 max-w-screen-2xl">
+                <Slider {...sliderSettings}>
+                    <div className="h-[500px] flex items-center justify-center bg-gray-200">
+                        <Image
+                            src="https://i.imgur.com/7LxsGtY.jpeg"
+                            alt="廣告 1"
+                            width={1200}
+                            height={300}
+                            className="object-cover w-full h-full"
+                            priority
+                        />
+                    </div>
+
+                    <div className="h-[500px] flex items-center justify-center bg-gray-200">
+                        <Image
+                            src="https://i.imgur.com/FhSWoZt.png"
+                            alt="廣告 2"
+                            width={1200}
+                            height={300}
+                            className="object-cover w-full h-full"
+                        />
+                    </div>
+
+                    <div className="h-[500px] flex items-center justify-center bg-gray-200">
+                        <Image
+                            src="https://i.imgur.com/V4j3d7Y.jpeg"
+                            alt="廣告 3"
+                            width={1200}
+                            height={300}
+                            className="object-cover w-full h-full"
+                        />
+                    </div>
+                </Slider>
+            </div>
+            <div className="container mx-auto px-4 py-8 mt-16">
                 <h1 className="text-4xl font-bold text-center mb-12">商品列表</h1>
 
                 {categories.map((category) => (
@@ -52,7 +125,7 @@ function Home({ data }: { data: Product[] }) {
                         <h2 className="text-2xl font-semibold mb-6 border-b-2 border-gray-300 pb-2">
                             {category}
                         </h2>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-4">
                             {groupedProducts[category]?.map((product, index) => (
                                 <ProductCard product={product} key={index} />
                             ))}
