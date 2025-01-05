44110c5d48a865acd7e98c34b629cb8ad0f02d3a
tyu <225jimmy@gmail.com>
2025-01-05 19:18:41 +0800

    fetch orders status update complete

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -25,7 +25,14 @@ function Admin() {
     const [pending, setPending] = useState<sampleOrder[]>([]);
     const [delivery, setDelivery] = useState<sampleOrder[]>([]);
     const [completed, setCompleted] = useState<sampleOrder[]>([]);
+    const [rejected, setRejected] = useState<sampleOrder[]>([]);
     const [draggedOrder, setDraggedOrder] = useState<sampleOrder>();
+    const [loading, setLoading] = useState(true);
+    
+    const statusOrder = [created, pending, delivery, completed, rejected];
+    const statusName = ['新訂單', '處理中', '運送中', '已完成', '已取消'];
+    const statusValue = ['created', 'pending', 'delivery', 'completed', 'rejected'];
+    const bgcolors = ['bg-purple-100', 'bg-purple-200', 'bg-purple-300', 'bg-purple-400', 'bg-red-400'];
 
     useEffect(() => {
         const token = localStorage.getItem('access-token');
@@ -99,6 +106,8 @@ function Admin() {
                         setDelivery(delivery => [...delivery, order]);
                     } else if(result.status == "completed") {
                         setCompleted(completed => [...completed, order]);
+                    } else if(result.status == "rejected") {
+                        setRejected(rejected => [...rejected, order]);
                     }
                 } else {
                     console.error('fetch order error:', response.status);
@@ -106,6 +115,7 @@ function Admin() {
             };
             fetchOrder();
         });
+        setLoading(false);
     }, [orders]);
 
     const fetchUser = async(email: string, name: string) => {
@@ -139,8 +149,6 @@ function Admin() {
         const date = moment(cleanedDateString, moment.ISO_8601);
         return date.isValid() ? date.format('YYYY-MM-DD HH:mm:ss') : 'Invalid date';
     };
-    const statusOrder = [created, pending, delivery, completed];
-
     const handleDragStart = (order:sampleOrder) => {
         setDraggedOrder(order);
     };
@@ -150,22 +158,25 @@ function Admin() {
             const updatedOrder = { ...draggedOrder, status };
             if (draggedOrder.status !== 'created' && status === 'created') {
                 setCreated([...created, updatedOrder]);
-                handleDropArray(draggedOrder.status);
+                handleDropArray(draggedOrder.status,status);
             } else if (draggedOrder.status !== 'pending' && status === 'pending') {
                 setPending([...pending, updatedOrder]);
-                handleDropArray(draggedOrder.status);
+                handleDropArray(draggedOrder.status,status);
             } else if (draggedOrder.status !== 'delivery' && status === 'delivery') {
                 setDelivery([...delivery, updatedOrder]);
-                handleDropArray(draggedOrder.status);
+                handleDropArray(draggedOrder.status,status);
             } else if (draggedOrder.status !== 'completed' && status === 'completed') {
                 setCompleted([...completed, updatedOrder]);
-                handleDropArray(draggedOrder.status);
+                handleDropArray(draggedOrder.status,status);
+            }else if (draggedOrder.status !== 'rejected' && status === 'rejected') {
+                setRejected([...rejected, updatedOrder]);
+                handleDropArray(draggedOrder.status,status);
             }
             setDraggedOrder(undefined);
         }
     };
 
-    const handleDropArray = (status:string) => {
+    const handleDropArray = (status:string,newStatus:string) => {
         if(draggedOrder){
             if(status === 'created') {
                 setCreated(created.filter(order => order.id !== draggedOrder.id));
@@ -175,8 +186,42 @@ function Admin() {
                 setDelivery(delivery.filter(order => order.id !== draggedOrder.id));
             }else if(status === 'completed') {
                 setCompleted(completed.filter(order => order.id !== draggedOrder.id));
+            } else if(status === 'rejected') {
+                setRejected(rejected.filter(order => order.id !== draggedOrder.id));
             }
         }
+        const fetchStatusUpdate = async () => {
+            const url = 'https://dongyi-api.hnd1.zeabur.app/order/api/status-upd';
+            const request = {
+                id: draggedOrder?.id,
+                admin: user?.id || '',
+                status: newStatus,
+            };
+            console.log('fetch status update request:', request);
+            try{
+                const response = await fetch(url, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(request),
+                });
+                if (response.ok) {
+                    const result = await response.json();
+                    console.log('fetch status update successfully:', result);
+                }
+                else{
+                    console.error('fetch status update error:', response.status);
+                }
+            } catch (error) {
+                console.error('fetch status update error:', error);
+            }
+        };
+        fetchStatusUpdate();
+    }
+
+    if (loading) {
+        return <Loading/>
     }
 
     return (
@@ -196,87 +241,29 @@ function Admin() {
             <div className="mt-4 px-6 mx-auto flex">
                 <div className="w-screen h-screen">
                     <h1 className="text-4xl font-bold flex-grow text-center">訂單管理</h1>
-                    <div className='flex space-x-4 mt-6 h-2/3'>
-                        <div className='w-1/4 bg-purple-100 shadow-md rounded-lg p-6 space-y-4'
-                            onDragOver={(e) => e.preventDefault()}
-                            onDrop={() => handleDrop('created')}>
-                            <h1 className='text-3xl font-bold text-center'>新訂單</h1>
-                            <div className='overflow-y-auto space-y-4 h-5/6'>
-                                {created.map((order,index) => (
-                                    <div className="bg-white shadow-md rounded-lg p-4 h-52" key={index}
-                                        draggable
-                                        onDragStart={() => handleDragStart(order)}>
-                                        <h1 className='text-lg font-bold'>訂單編號: {order.id}</h1>
-                                        <h1 className='text-lg font-bold'>買家: {order.owner}</h1>
-                                        <h1 className='text-lg font-bold'>訂單日期: {formatDate(order.created_at)}</h1>
-                                        <Link href={`/order?id=${order.id}`}>
-                                            <button className='bg-purple-400 text-white py-2 px-4 rounded'>查看訂單</button>
-                                        </Link>
-                                    </div>
-                                ))}
+                    <div className='flex space-x-4 mt-6 h-2/3 w-full'>
+                        {statusOrder.map((order,index) => (
+                            <div className={`w-1/5 shadow-md rounded-lg p-3 space-y-4 ${bgcolors[index]}`} key={index}
+                                onDragOver={(e) => e.preventDefault()}
+                                onDrop={() => handleDrop(statusValue[index])}>
+                                <h1 className='text-3xl font-bold text-center'>{statusName[index]}</h1>
+                                <div className='overflow-y-auto space-y-4 h-5/6'>
+                                    {order.map((order,index) => (
+                                        <div className="bg-white shadow-md rounded-lg p-3 h-52" key={index}
+                                            draggable
+                                            onDragStart={() => handleDragStart(order)}>
+                                            <h1 className='text-lg font-bold'>訂單編號: {order.id}</h1>
+                                            <h1 className='text-lg font-bold'>買家: </h1>
+                                            <h1 className='text-lg font-bold'>{order.owner}</h1>
+                                            <h1 className='text-lg font-bold'>訂單日期: {formatDate(order.created_at)}</h1>
+                                            <Link href={`/order?id=${order.id}`}>
+                                                <button className='bg-purple-400 text-white py-2 px-4 rounded'>查看訂單</button>
+                                            </Link>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-
-                        <div className='w-1/4 bg-purple-200 rounded-lg p-6 flex flex-col space-y-4'
-                            onDragOver={(e) => e.preventDefault()}
-                            onDrop={() => handleDrop('pending')}>
-                            <h1 className='text-3xl font-bold text-center'>處理中</h1>
-                            <div className='overflow-y-auto space-y-4 h-5/6'>
-                                {pending.map((order,index) => (
-                                    <div className="bg-white shadow-md rounded-lg p-4 h-52" key={index}
-                                        draggable
-                                        onDragStart={() => handleDragStart(order)}>
-                                        <h1 className='text-lg font-bold'>訂單編號: {order.id}</h1>
-                                        <h1 className='text-lg font-bold'>買家: {order.owner}</h1>
-                                        <h1 className='text-lg font-bold'>訂單日期: {formatDate(order.created_at)}</h1>
-                                        <Link href={`/order?id=${order.id}`}>
-                                            <button className='bg-purple-400 text-white py-2 px-4 rounded'>查看訂單</button>
-                                        </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className='w-1/4 bg-purple-300 rounded-lg p-6 flex flex-col space-y-4'
-                            onDragOver={(e) => e.preventDefault()}
-                            onDrop={() => handleDrop('delivery')}>
-                            <h1 className='text-3xl font-bold text-center'>運送中</h1>
-                            <div className='overflow-y-auto space-y-4 h-5/6'>
-                                {delivery.map((order,index) => (
-                                    <div className="bg-white shadow-md rounded-lg p-4 h-52" key={index}
-                                        draggable
-                                        onDragStart={() => handleDragStart(order)}>
-                                        <h1 className='text-lg font-bold'>訂單編號: {order.id}</h1>
-                                        <h1 className='text-lg font-bold'>買家: {order.owner}</h1>
-                                        <h1 className='text-lg font-bold'>訂單日期: {formatDate(order.created_at)}</h1>
-                                        <Link href={`/order?id=${order.id}`}>
-                                            <button className='bg-purple-400 text-white py-2 px-4 rounded'>查看訂單</button>
-                                        </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className='w-1/4 bg-purple-400 rounded-lg p-6 flex flex-col space-y-4'
-                            onDragOver={(e) => e.preventDefault()}
-                            onDrop={() => handleDrop('completed')}>
-                            <h1 className='text-3xl font-bold text-center'>已完成</h1>
-                            <div className='overflow-y-auto space-y-4 h-5/6'>
-                                {completed.map((order,index) => (
-                                    <div className="bg-white shadow-md rounded-lg p-4 h-52" key={index}
-                                        draggable
-                                        onDragStart={() => handleDragStart(order)}>
-                                        <h1 className='text-lg font-bold'>訂單編號: {order.id}</h1>
-                                        <h1 className='text-lg font-bold'>買家: {order.owner}</h1>
-                                        <h1 className='text-lg font-bold'>訂單日期: {formatDate(order.created_at)}</h1>
-                                        <Link href={`/order?id=${order.id}`}>
-                                            <button className='bg-purple-400 text-white py-2 px-4 rounded'>查看訂單</button>
-                                        </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className='w-1/4 bg-red-300 shadow-md rounded-lg p-6'>
-                            <h1 className='text-3xl font-bold text-center'>已取消</h1>                  
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
